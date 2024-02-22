09f659f762f721cfdc7eba31d2f39479598aa90f
Cisco Vlahakis <ciscovlahakis@gmail.com>
2024-02-22 18:36:36 +0000

    completed tabbed interface

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by!(username: params.fetch(:username))
   end
+
+  def liked
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def following
+    @user = User.find_by!(username: params.fetch(:username))
+
+    render "users_list", locals: {users_list: @user.leaders}
+  end
+
+  def followers
+    @user = User.find_by!(username: params.fetch(:username))
+
+    render "users_list", locals: {users_list: @user.followers}
+  end
 end
